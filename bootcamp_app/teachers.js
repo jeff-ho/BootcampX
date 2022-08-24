0656ae6ffa78b32d1d8ba77c2a507b86cbbf2b0a
Jeffrey Ho <jeff.ho@hotmail.com>
2022-08-24 21:56:25 +0000

    Add student and teachers node.js PSQL connection and queries

---
+++ bootcamp_app/teachers.js
@@ -0,0 +1,28 @@
+const { query } = require('express');
+const { Pool } = require('pg');
+let arg = process.argv[2];
+let max = process.argv[3];
+
+const pool = new Pool({
+  user: 'vagrant',
+  password: '123',
+  host: 'localhost',
+  database: 'bootcampx'
+});
+
+pool.query (`
+SELECT DISTINCT teachers.name as teacher, cohorts.name as cohort
+FROM assistance_requests
+JOIN students ON student_id = students.id
+JOIN cohorts ON cohort_id = cohorts.id
+JOIN teachers ON teacher_id = teachers.id
+WHERE cohorts.name = '${arg}'
+ORDER BY teacher;`)
+.then(results => {
+  results.rows.forEach(teacher => {
+    console.log(`${teacher.cohort}: ${teacher.teacher}`)
+  })
+})
+.catch(error => {
+  console.log('Error')
+});
